a330a09035cbdf5f894a0146db02cea4ea72199c
Willy Gutsche <willy.gutsche@unifesp.br>
2023-11-29 11:24:44 -0300

    20 - Valid Parentheses

---
+++ easy/20-valid-parentheses.ts
@@ -0,0 +1,29 @@
+function isValid(s: string): boolean {
+  const stack: string[] = []
+
+  const openParentheses = ['(', '[', '{']
+
+  const closeAndOpenParentheses = {
+    ')': '(', 
+    ']': '[', 
+    '}': '{'
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    const currentParentheses = s.charAt(i)
+
+    if (openParentheses.includes(currentParentheses)) {
+      stack.push(currentParentheses)
+      continue
+    }
+    
+    const headOfStack = stack.pop()
+    const parenthesesToMatch = closeAndOpenParentheses[currentParentheses]
+
+    if (headOfStack !== parenthesesToMatch) {
+      return false
+    }
+  }
+
+  return stack.length === 0
+};
