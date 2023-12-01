4441f214b21ee55936686cb231ff789224fddbee
Willy Gutsche <willy.gutsche@unifesp.br>
2023-12-01 12:47:35 -0300

    Add ESlint and set style files

--- easy/13-roman-to-integer.ts
+++ easy/13-roman-to-integer.ts
@@ -2,21 +2,21 @@ function romanToInt(s: string): number {
   let value = 0;
 
   for (let i = 0; i < s.length; i++) {
-      const currentLetter = s.charAt(i);
+    const currentLetter = s.charAt(i);
 
-      if (checkNextLetter(s, i)) {
-          const nextLetter = s.charAt(i+1);
-          value += romanNumerals[nextLetter] - romanNumerals[currentLetter];
-          i++;
-      } else {
-          value += romanNumerals[currentLetter];
-      }
+    if (checkNextLetter(s, i)) {
+      const nextLetter = s.charAt(i+1);
+      value += romanNumerals[nextLetter] - romanNumerals[currentLetter];
+      i++;
+    } else {
+      value += romanNumerals[currentLetter];
+    }
   }
 
   return value;
-};
+}
 
-const romanNumerals = {
+const romanNumerals: {[key: string]: number} = {
   I:             1,
   V:             5,
   X:             10,
@@ -28,22 +28,22 @@ const romanNumerals = {
 
 const checkNextLetter = (s: string, currentIndex: number): boolean => {
   if (currentIndex >= s.length - 1) {
-      return false;
+    return false;
   }
 
   const currentLetter = s.charAt(currentIndex);
   const nextLetter = s.charAt(currentIndex + 1);
 
   if (currentLetter === 'I') {
-      return ['V', 'X'].includes(nextLetter);
+    return ['V', 'X'].includes(nextLetter);
   }
 
   if (currentLetter === 'X') {
-      return ['L', 'C'].includes(nextLetter);
+    return ['L', 'C'].includes(nextLetter);
   }
 
   if (currentLetter === 'C') {
-      return ['D', 'M'].includes(nextLetter);
+    return ['D', 'M'].includes(nextLetter);
   }
 
   return false;
--- easy/9-palindrome-number.ts
+++ easy/9-palindrome-number.ts
@@ -1,16 +1,16 @@
 function isPalindrome(x: number): boolean {
   if (x < 0) {
-      return false;
+    return false;
   }
 
   const numberToString = x.toString();
   const length = numberToString.length;
 
   for (let i = 0; i < length / 2; i++) {
-      if (numberToString[i] !== numberToString[length - 1 - i]) {
-          return false;
-      }
+    if (numberToString[i] !== numberToString[length - 1 - i]) {
+      return false;
+    }
   }
 
   return true;
-};
+}
